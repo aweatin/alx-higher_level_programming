a575fa6fa5917d52973ba7ee9d38a3f252187146
aweatin <genesisawe@gmail.com>
2023-09-09 10:46:38 -0700

    first addimport

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,6 @@
-#!/urs/bin/python3
-import add_0
-add_0.add(1, 2)
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
