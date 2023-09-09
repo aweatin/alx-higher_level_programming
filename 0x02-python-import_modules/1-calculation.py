c96d4068a069f183054a0b36bda76cc0aac7427f
aweatin <genesisawe@gmail.com>
2023-09-09 11:31:25 -0700

    cal import1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculate_1 import add
-    from calculate_1 import sub
-    from calculate_1 import mul
-    from calculate_1 import div
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
