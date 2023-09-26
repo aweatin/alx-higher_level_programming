554c89b38b435a6e9f1d3b02e59d448c0737e3b3
aweatin <genesisawe38@gmail.com>
2023-09-26 05:33:08 -0700

    adding comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,15 @@ Module 1-square
 Define class Square with private attribute size
 """
 
+
 class Square:
+    """Represent s squqre."""
+
     def __init__(self, size):
+        """
+        Initializes square
+
+        Attributes:
+            size: size of a square
+        """
         self.__size = size
